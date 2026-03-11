23dbfad0f66c261986efd2762d3aa07cfc108d33
Ronnie Turner <ronnie.turner.303@gmail.com>
2026-03-11 07:38:14 -0600

    Added random number generation, still require comparison fuction tba

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::io;
 
+use rand::RngExt;
 // comment like so
-    
+
 fn main() {
 
     println!("Guess the number!");
+
+    let mut rng = rand::rng();
+    
+    let secret_number = rng.random_range(1..=101);
+
+    println!("The secret number is {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
